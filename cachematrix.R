18979a732f7ea1a7b83a6bca47b3f68697cdc218
Henry Hermawan <hevi2001@gmail.com>
2015-02-21 23:43:08 +0700

    writing the makeCacheMatrix() and cacheSolve() functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix() function is used to set up a list that contains a matrix of x and 
+## its inverse
+## Example of set up a matrix:
+##      p1 <- makeCacheMatrix()
+##      p1$set(matrix(1:4,2,2))
+## When typing p1$get(), we will get:
+##           [,1] [,2]
+##      [1,]    1    3
+##      [2,]    2    4
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve() function will return the inverse of matrix x
+## Just type: cacheSolve(x)
+## Example:
+##      p1 <- makeCacheMatrix()
+##      p1$set(matrix(1:4,2,2))
+##      cacheSolve(p1)
+## When typing p1$getinv(), we will get:
+##           [,1] [,2]
+##      [1,]   -2  1.5
+##      [2,]    1 -0.5
+## For proving, type p1$getinv()%*%p1$get(), we will get the identity matrix:
+##           [,1] [,2]
+##      [1,]    1    0
+##      [2,]    0    1
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
